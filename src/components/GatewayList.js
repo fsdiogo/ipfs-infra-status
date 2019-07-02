ef88ba4945b0dc732049214426f4d42f1c8b6a2f
Diogo Silva <fsdiogo@gmail.com>
2019-07-02 14:24:25 +0100

    chore: update table

--- src/components/GatewayList.js
+++ src/components/GatewayList.js
@@ -4,16 +4,14 @@ import GATEWAYS from '../../static/gateways.json'
 
 const TEST_HASH = 'Qmaisz6NMhDB51cCvNWa1GMS7LU1pAxdF4Ld6Ft9kZEP2a'
 
-const Gateway = ({ host }) => {
+const Gateway = ({ gatewayUrl }) => {
   const [online, setOnline] = useState(false)
-  const [gatewayWithHash, setGatewayWithHash] = useState('')
   const [rtt, setRtt] = useState(0)
 
   useEffect(() => {
-    const gatewayAndHash = host.replace(':hash', TEST_HASH)
-    setGatewayWithHash(gatewayAndHash)
+    const gatewayWithHash = `${gatewayUrl}/ipfs/${TEST_HASH}`
     // opt-out from gateway redirects done by browser extension
-    const testUrl = gatewayAndHash + '#x-ipfs-companion-no-redirect'
+    const testUrl = gatewayWithHash + '#x-ipfs-companion-no-redirect'
     const start = performance.now()
 
     fetch(testUrl)
@@ -27,40 +25,35 @@ const Gateway = ({ host }) => {
       })
   }, [])
 
+  const gateway = gatewayUrl.split('https://')[1].split('/ipfs/:hash')[0]
   const dataClass = 'pv2 ph3 bt b--near-white charcoal'
 
   return (
-    online
-      ? <tr>
-        <td className={dataClass}>✅ Online</td>
-        <td className={dataClass}><a className='link blue' href={gatewayWithHash} target='_blank'>{gatewayWithHash}</a></td>
-        <td className={dataClass}>{rtt}ms</td>
-      </tr>
-      : <tr>
-        <td className={dataClass}>❌ Offline</td>
-        <td className={dataClass}>{gatewayWithHash}</td>
-        <td className={dataClass}>-</td>
-      </tr>
+    <tr>
+      <td className={dataClass}>{online ? '✅ Online' : '❌ Offline'}</td>
+      <td className={dataClass}><a className='link blue' href={gatewayUrl} target='_blank'>{gateway}</a></td>
+      <td className={dataClass}>{online ? `${rtt}ms` : '-'}</td>
+    </tr>
   )
 }
 
 const GatewayList = () => {
-  const tableClass = 'w-100 collapse sans-serif'
-  const headerClass = 'aqua fw2 ttu tl tracked pv2 ph3'
+  const tableClass = 'w-100 w-80-ns mt4 center collapse sans-serif'
+  const headerClass = 'fw6 tl tracked pv2 ph3'
 
   return (
-    <div className='h-100 ma3 flex flex-column align-center'>
+    <div className='h-100 flex flex-column align-center'>
       <table className={tableClass}>
         <tbody>
           <tr>
-            <th className={headerClass}>Status</th>
-            <th className={headerClass}>Host</th>
-            <th className={headerClass}>RTT</th>
+            <td className={headerClass}>Status</td>
+            <td className={headerClass}>Gateway</td>
+            <td className={headerClass}>Response time</td>
           </tr>
           { GATEWAYS.map((gateway, idx) =>
             <Gateway
               key={`gateway-${idx}`}
-              host={gateway} />
+              gatewayUrl={gateway} />
           ) }
         </tbody>
       </table>
