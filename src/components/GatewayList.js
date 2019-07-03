eb4bd6deb7fcab10e4745268994d708f9d2793d8
Diogo Silva <fsdiogo@gmail.com>
2019-07-03 15:19:10 +0100

    feat: add badges to th

--- src/components/GatewayList.js
+++ src/components/GatewayList.js
@@ -4,7 +4,7 @@ import GATEWAYS from '../../static/gateways.json'
 
 const TEST_HASH = 'Qmaisz6NMhDB51cCvNWa1GMS7LU1pAxdF4Ld6Ft9kZEP2a'
 
-const Gateway = ({ gatewayUrl }) => {
+const Gateway = ({ gatewayUrl, incrementChecked, incrementOnline }) => {
   const [online, setOnline] = useState(false)
   const [rtt, setRtt] = useState(0)
 
@@ -19,9 +19,13 @@ const Gateway = ({ gatewayUrl }) => {
       .then(() => {
         const rtt = Math.round(performance.now() - start)
         setOnline(true)
+        incrementOnline()
         setRtt(rtt)
       }).catch(() => {
         setOnline(false)
+      })
+      .finally(() => {
+        incrementChecked()
       })
   }, [])
 
@@ -37,7 +41,19 @@ const Gateway = ({ gatewayUrl }) => {
   )
 }
 
+const Badge = ({ checked, total = 0, online = null }) => (
+  online
+    ? <span className='ml1 pv1 ph2 white br-pill f7' style={{ backgroundColor: '#00b500' }} title='Online gateways'>{checked}</span>
+    : <span className='ml1 pv1 ph2 bg-navy white br-pill f7' title='Checked gateways'>{checked}/{total}</span>
+)
+
 const GatewayList = () => {
+  const [checked, setChecked] = useState(0)
+  const [online, setOnline] = useState(0)
+
+  const incrementChecked = () => setChecked(checked => checked + 1)
+  const incrementOnline = () => setOnline(online => online + 1)
+
   const tableClass = 'w-100 w-80-ns mv4 center collapse sans-serif'
   const headerClass = 'fw6 tl tracked pv2 ph3'
 
@@ -46,14 +62,16 @@ const GatewayList = () => {
       <table className={tableClass}>
         <tbody>
           <tr>
-            <td className={headerClass}>Status</td>
-            <td className={headerClass}>Gateway</td>
+            <td className={headerClass}>Status <Badge checked={online} online /></td>
+            <td className={headerClass}>Gateway <Badge checked={checked} total={GATEWAYS.length} /></td>
             <td className={headerClass}>Response time</td>
           </tr>
           { GATEWAYS.map((gateway, idx) =>
             <Gateway
               key={`gateway-${idx}`}
-              gatewayUrl={gateway} />
+              gatewayUrl={gateway}
+              incrementChecked={incrementChecked}
+              incrementOnline={incrementOnline} />
           ) }
         </tbody>
       </table>
